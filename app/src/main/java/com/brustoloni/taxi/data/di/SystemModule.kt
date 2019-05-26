5e0781273c9533014127f8fb1a894787fb875cd1
Bruno Brustoloni e Oliveira <brustoloni@ciandt.com>
2019-05-26 20:05:16 -0300

    fix deprecated calls

--- app/src/main/java/com/brustoloni/taxi/data/di/SystemModule.kt
+++ app/src/main/java/com/brustoloni/taxi/data/di/SystemModule.kt
@@ -1,8 +1,6 @@
 package com.brustoloni.taxi.data.di
 
-import android.content.Context
 import com.squareup.moshi.Moshi
-import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module.module
 
 
